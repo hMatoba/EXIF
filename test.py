22f30a9472d14bd187a9956f1e59988daa556325
hMatoba <g838.mahiroo@gmail.com>
2014-09-27 20:17:31 +0900

    Added scripts.
    Added module script and test.

---
+++ test.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+
+from PIL import Image
+from EXIF import Exif
+
+
+class ExifTests(unittest.TestCase):
+    def test_roundtrip(self):
+        d = {11: "PIL",
+             271: "Make",
+             272: "XXX-XXX",
+             282: (300, 1),
+             283: (200, 1),
+             18246: 3,
+             36867: "2099:09:29 10:10:10",
+             41994: 1,
+             42035: "LensMake",
+             34853: {0: 2, 2: (1, 300), 29: "1999:99:99 99:99:99"}}
+        exif = Exif(d)
+        exif.load(exif.to_bytes())
+        exif.pop(34665) # Exif IFD pointer
+        self.assertDictEqual(exif, d)
+
+    def test_file_generate(self):
+        d = {11: "PIL",
+             271: "Make",
+             272: "XXX-XXX",
+             282: (300, 1),
+             283: (200, 1),
+             18246: 3,
+             36867: "2099:09:29 10:10:10",
+             41994: 1,
+             42035: "LensMake",
+             34853: {0: 2, 2: (1, 300), 29: "1999:99:99 99:99:99"}}
+        exif = Exif(d)
+        im1 = Image.new("RGBA", (16, 16))
+        im1.save("im1.jpg", exif=exif.to_bytes())
+        im2 = Image.open("im1.jpg")
+        im2.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
