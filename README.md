3114b13d855a0735efa591bf9d0a3bebe4ad5e64
hMatoba <g838.mahiroo@gmail.com>
2014-10-01 11:02:11 +0900

    Added classes support to make exif dict.
    Classes 'ImageGroup', 'PhotoGroup' and 'GPSGroup' were added.

--- README.md
+++ README.md
@@ -7,17 +7,19 @@ Use Exif object with PIL or Pillow.
 How to Use
 --------
     from PIL import Image
-    from EXIF import Exif
+    from EXIF import *
 
-    d = {11: "PIL",
-         272: "XXX-XXX",
-         282: (300, 1),
-         283: (200, 1),
-         18246: 3,
-         36867: "2099:09:29 10:10:10",
-         41994: 1,
-         42035: "LensMake",
-         34853: {0: 2, 2: (1, 300), 29: "1999:99:99 99:99:99"}}
+    d = {ImageGroup.ProcessingSoftware: "PIL",
+         ImageGroup.Model: "XXX-XXX",
+         ImageGroup.XResolution: (300, 1),
+         ImageGroup.YResolution: (200, 1),
+         ImageGroup.Rating: 3,
+         PhotoGroup.DateTimeOriginal: "2099:09:29 10:10:10",
+         PhotoGroup.Sharpness: 1,
+         PhotoGroup.LensMake: "LensMake",
+         ImageGroup.GPSTag: {GPSGroup.GPSVersionID: 2,
+                             GPSGroup.GPSLatitude: (1, 300),
+                             GPSGroup.GPSDateStamp: "1999:99:99 99:99:99"}}
     exif = Exif()
     exif.update(d)
     exif[271] = "Make"
--- test.py
+++ test.py
@@ -2,21 +2,23 @@ import os
 import unittest
 
 from PIL import Image
-from EXIF import Exif
+from EXIF import *
 
 
 class ExifTests(unittest.TestCase):
     def test_roundtrip(self):
-        d = {11: "PIL",
-             271: "Make",
-             272: "XXX-XXX",
-             282: (300, 1),
-             283: (200, 1),
-             18246: 3,
-             36867: "2099:09:29 10:10:10",
-             41994: 1,
-             42035: "LensMake",
-             34853: {0: 2, 2: (1, 300), 29: "1999:99:99 99:99:99"}}
+        d = {ImageGroup.ProcessingSoftware: "PIL",
+             ImageGroup.Make: "Make",
+             ImageGroup.Model: "XXX-XXX",
+             ImageGroup.XResolution: (300, 1),
+             ImageGroup.YResolution: (200, 1),
+             ImageGroup.Rating: 3,
+             PhotoGroup.DateTimeOriginal: "2099:09:29 10:10:10",
+             PhotoGroup.Sharpness: 1,
+             PhotoGroup.LensMake: "LensMake",
+             ImageGroup.GPSTag: {GPSGroup.GPSVersionID: 2,
+                                 GPSGroup.GPSLatitude: (1, 300),
+                                 GPSGroup.GPSDateStamp: "1999:99:99 99:99:99"}}
         exif = Exif(d)
         exif.load(exif.to_bytes())
         exif.pop(34665) # Exif IFD pointer
