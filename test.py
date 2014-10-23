11c601841d474f09c906b6e404f7781ac5fe8983
hMatoba <g838.mahiroo@gmail.com>
2014-10-23 22:12:57 +0900

    Test push for travis ci.

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ from EXIF import Exif, GPSGroup, ImageGroup, PhotoGroup
 EXIF_DICT = {
     ImageGroup.ProcessingSoftware: "PIL",  # ascii
     ImageGroup.Make: "Make",  # ascii
+    ImageGroup.Artist: "Mr. Foo",
     ImageGroup.Model: "XXX-XXX",  # ascii
     ImageGroup.ClipPath: 255,  # byte
     ImageGroup.Rating: 65535,  # short
